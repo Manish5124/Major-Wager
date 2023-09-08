3d66464189ed42b6c0372fc51777b90a9ebccc10
manish5124 <manishchouhan5124@gmail.com>
2023-09-08 15:05:30 +0530

    Eureka registry service

---
+++ Backend/Eureka-server/src/test/java/com/cts/urekaserver/UrekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.urekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
